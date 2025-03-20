87aa3777db90c71695390a838f0982bd02df552b
jeffreyvanelderen <jeffrey_vanelderen@hotmail.com>
2025-03-20 13:23:50 +0100

    Add switch control flow, add implicit for loop variables, add reference to enum in template, add explanation for track in for

--- src/app/conditionals/conditionals.component.ts
+++ src/app/conditionals/conditionals.component.ts
@@ -8,12 +8,29 @@ import { Component } from '@angular/core';
 })
 export class ConditionalsComponent {
 
+  /*
+  This exposes the Exam enum to the TEMPLATE! so you can use Exam.X in the template SWITCH cases instead of using string literals!
+  */
+  Exam = Exam;
+
   hasPassedExam: boolean = false;
-  exams: string[] = ["Math", "Programming", "Dutch", "PE"];
+  exams: Exam[] = Object.values(Exam);
   prediction: string = '';
+
+  currentExam: Exam = Exam.Math;
 
   takeExam() {
     this.hasPassedExam = !this.hasPassedExam;
+    
+    const newIndex = this.exams.indexOf(this.currentExam) + 1;
+    this.currentExam = this.exams[newIndex >= this.exams.length ? 0: newIndex];
   }
+  
+}
 
+enum Exam {
+  "Math" = "Math", 
+  "Programming" = "Programming", 
+  "Dutch" = "Dutch", 
+  "PE" = "PE",
 }
