f9aa395e180a48c4573f661ff404b1623b9b7f6b
jeffreyvanelderen <jeffrey_vanelderen@hotmail.com>
2025-03-26 14:25:46 +0100

    Add Angular Material input example

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,10 +1,12 @@
 import {Component, ElementRef, Input, ViewChild} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { OnlyLettersValidatorDirective } from './validator/only-letters-validator.directive';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-template-form',
-  imports: [FormsModule, OnlyLettersValidatorDirective],
+  imports: [FormsModule, OnlyLettersValidatorDirective, MatInputModule, MatFormFieldModule],
   templateUrl: './template-form.component.html',
   styleUrl: './template-form.component.scss',
 })
