400ed24fa543be2e9cadd75381865d414f2660a8
jeffreyvanelderen <jeffrey_vanelderen@hotmail.com>
2025-04-08 15:27:47 +0200

    Clean up imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,8 @@
 import {Component} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import {InputOutputComponent} from './input-output/input-output.component';
-import {ControlFlowsComponent} from './control-flows/control-flows.component';
-import {DividerComponent} from './divider/divider.component';
-import {DeferrableComponent} from './deferrable/deferrable.component';
-import {OptimizedImageComponent} from './optimized-image/optimized-image.component';
-import {TemplateFormComponent} from './template-form/template-form.component';
-import {ReactiveFormComponent} from './reactive-form/reactive-form.component';
-import {PipeUsageComponent} from './pipe-usage/pipe-usage.component';
-import {ResponsiveItemComponent} from './responsive-item/responsive-item.component';
-import {SignalComponent} from './signal/signal.component';
-import {AttributeComponent} from './attribute/attribute.component';
-import {UsingNgContentComponent} from './using-ng-content/using-ng-content.component';
-import {TemplateFragmentComponent} from './template-fragment/template-fragment.component';
-import {UsingNgContainerComponent} from './using-ng-container/using-ng-container.component';
 import {
-  NgContainerDirectiveComponent,
   Theme,
 } from './ng-container-directive/ng-container-directive.component';
-import {VariablesInTemplatesComponent} from './variables-in-templates/variables-in-templates.component';
-import {NgClassDirectiveComponent} from './ng-class-directive/ng-class-directive.component';
-import {NgStyleDirectiveComponent} from './ng-style-directive/ng-style-directive.component';
-import {NgModelDirectiveComponent} from './ng-model-directive/ng-model-directive.component';
-import {HighlightDirectiveDirective} from '../directives/highlight-directive/highlight-directive.directive';
-import {MouseDetectorDirectiveDirective} from '../directives/mouse-detector-directive/mouse-detector-directive.directive';
 import {StorageService} from '../services/storage/storage.service';
 
 @Component({
